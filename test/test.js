488ab92368c8486257ecfeaf7e87bb20bbb9ebf9
David Pereira <david.pereira@dmfec.local>
2019-06-21 16:58:26 -0300

    Creating a sample test

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var supertest = require('supertest')
+var should = require('should')
+
+var server = supertest.agent('http://localhost:3000')
+
+describe('SAMPLE unit test', () => {
+
+    it('should return home page', done => {
+
+        server
+            .get('/')
+            .expect('Content-Type', /text/)
+            .expect(200)
+            .end((err, res) => {
+
+                res.status.should.equal(200)
+                done()
+            })
+    })
+})
